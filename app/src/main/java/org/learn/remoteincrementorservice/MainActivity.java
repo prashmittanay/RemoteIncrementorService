daf4ebed3202384fb07a49fb4950fe36b8bc227d
Prashmit Tanay <prashmittanay@gmail.com>
2020-10-04 19:46:50 +0530

    service stopped onDestroy of main activity

--- app/src/main/java/org/learn/remoteincrementorservice/MainActivity.java
+++ app/src/main/java/org/learn/remoteincrementorservice/MainActivity.java
@@ -1,14 +1,24 @@
 package org.learn.remoteincrementorservice;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Intent mServiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mServiceIntent = new Intent(this, IncrementorService.class);
+        startService(mServiceIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(mServiceIntent);
     }
 }
