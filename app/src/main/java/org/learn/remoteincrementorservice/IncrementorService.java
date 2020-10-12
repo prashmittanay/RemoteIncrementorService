ff1bfb3ddd614266200d748f061c5dea0a5148e9
Prashmit Tanay <prashmittanay@gmail.com>
2020-10-12 22:28:13 +0530

    activities removed!

--- app/src/main/java/org/learn/remoteincrementorservice/IncrementorService.java
+++ app/src/main/java/org/learn/remoteincrementorservice/IncrementorService.java
@@ -5,9 +5,22 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.util.Log;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class IncrementorService extends Service {
+    private static final String TAG = "IncrementorService";
     private int mIntValue = 0;
+    private IncrementorClass mIncrementorClass;
     public IncrementorService() {
     }
 
@@ -26,8 +39,16 @@ public class IncrementorService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        IncrementorClass incrementorClass = new IncrementorClass();
-        incrementorClass.execute();
+        Log.d(TAG, "Remote Incrementor Service Created!");
+        mIncrementorClass = new IncrementorClass();
+        mIncrementorClass.execute();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mIncrementorClass.cancel(true);
+        Log.d(TAG, "Remote Incrementor Service Destroyed!");
     }
 
     private class IncrementorClass extends AsyncTask {
@@ -35,13 +56,12 @@ public class IncrementorService extends Service {
         protected Object doInBackground(Object[] objects) {
             try {
                 while (true) {
-                    Thread.sleep(900);
+                    Thread.sleep(20);
                     mIntValue++;
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
             return null;
         };
     }
